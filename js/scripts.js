e7ab8fbd24f7337ff63402b056b734a2e14cf6f1
Justin Eder <justineder@gmail.com>
2017-04-24 10:43:35 -0500

    fixed slide-down button bug

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 (function($) {
 
 	$(document).ready(function(){
-        
+
         $('.pre-load').removeClass('pre-load');
-        
+
 	  $(".new-carousel").owlCarousel({
 	  	navText:{},
 	  	responsiveClass:true,
@@ -77,23 +77,23 @@
         // Add 50px to height because there are a combined 50px of borders added on to the element
         // and since box-sizing: border-box; we need to account for that.
         $height = $this.height() + 50;
-        
+
         $(this).find('.fa').removeClass('fa-plus').addClass('fa-minus');
         $container.addClass('expandable-box-expanded');
         $parent.css('height',$height);
     });
-    
+
     $(document).on('click','.expandable-box-expanded',function() {
         $(this).find('.fa').removeClass('fa-minus').addClass('fa-plus');
         $(this).removeClass('expandable-box-expanded');
         $(this).find('.expandable-box-content').css('height','0');
     })
-    
+
     $searchform = '<div class="search-close"></div><div class="container"><div class="row"><form action="/" method="get"><label class="screen-reader-text" for="search">Search</label><input type="text" class="search-field" name="s" placeholder="Search" id="search" value="" autocomplete="off" /></form></div></div>';
-    
+
     $(document).on('click', '#menu-item-32', function(e) {
         if( $(this).hasClass('search-expanded') ) {
-            // Do nothing   
+            // Do nothing
         } else {
             $(this).addClass('search-expanded');
             $('<div class="search-container"></div>').appendTo('header').hide().append($searchform).fadeIn();
@@ -102,7 +102,7 @@
         }
         e.preventDefault();
     });
-    
+
     $(document).on('click', '.search-expanded, .search-close', function(e) {
         $('.search-container').fadeOut(300, function() {
             $(this).remove();
@@ -111,26 +111,26 @@
         $('html').removeClass('fixed');
         e.preventDefault();
     });
-    
+
     $(document).on('click', '.btn-modal-trigger', function(e) {
         $('.hidden-form').fadeIn();
         $('html').addClass('fixed');
-        
+
         e.preventDefault();
     });
-    
+
     $(document).on('click', '.modal-close', function() {
         $(this).parent().fadeOut();
         $('html').removeClass('fixed');
     });
-    
+
     $(document).on('click', '.btn-replace-trigger', function(e) {
         $form = $('.hidden-form').html();
         $(this).parent().parent().html($form);
-        
+
         e.preventDefault();
     });
-    
+
     $(document).on('submit', '.page-template-page-shop-online .page-content form', function(e) {
         $query = $('form .magento-search').val();
         $store = 'http://paradigm.emcp.com/catalogsearch/result/?';
@@ -138,39 +138,37 @@
         window.open( $store + $.param($params), '_blank');
 
         e.preventDefault();
-    });   
-    
-    // IE Fix 
+    });
+
+    // IE Fix
     $(document).on('keypress', '.page-template-page-shop-online .page-content form', function(event){
          if(event.keyCode == 13){
              $('.page-template-page-shop-online .page-content form').submit();
          }
      });
-    
+
+
     $(document).on('click', '.button-slide', function(e) {
         $(this).addClass('button-collapse');
         $(this).find('i').removeClass('fa-plus').addClass('fa-minus');
         $slideId = $(this).attr('id');
-        
         $elem = $('.row-hidden .slide-content-' + $slideId);
         $slideHeight = $elem.height();
-        
+
         $slideContainer = $elem.parent().parent().parent();
         $slideContainer.css('height',$slideHeight);
-        e.preventDefault();        
+        e.preventDefault();
     });
-    
+
     $(document).on('click', '.button-collapse', function(e) {
         $(this).removeClass('button-collapse');
         $(this).find('i').removeClass('fa-minus').addClass('fa-plus');
         $slideId = $(this).attr('id');
-        
         $elem = $('.row-hidden .slide-content-' + $slideId);
-        
         $slideContainer = $elem.parent().parent().parent();
         $slideContainer.css('height','0');
-        
-        e.preventDefault();        
+
+        e.preventDefault();
     });
-    
+
 })( jQuery );
--- template-parts/content-alternating-text-images.php
+++ template-parts/content-alternating-text-images.php
@@ -15,57 +15,73 @@
             <?php
 
             if( have_rows('slice') ):
-            
+
                 $rows = get_field('slice');
                 $count = count($rows);
-                
+
                 for( $i = 0; $i < $count; $i++ ) {
-                    
+
                     $content = $rows[$i]['content'];
                     $image = $rows[$i]['image'];
-                    
-                    $buttons = $rows[$i]['buttons'];                    
+
+                    $buttons = $rows[$i]['buttons'];
                     $button_count = count($buttons);
-                    
+
                     $button_html = '';
                     $slide_down_content = '';
-                    
+
                     $img = '';
                     if( !empty($image) ) {
                         $img = '<img src="'. $rows[$i]['image'] .'" />';
                     }
-                    
+
                     if($buttons['0']['button_text'] == '') {
-                        
+
                     } else {
-                        
+
                         for( $ii = 0; $ii < $button_count; $ii++ ) {
 
                             if( $buttons[$ii]['target'] == 'slide' ) {
 
-                                // Create unique ids for collapsable button elements -- currently not selecting height element
-                                // $button_html .= '<a class="button button-slide" id="'. $i .'-'. $ii .'">
-                                $button_html .= '<a class="button button-slide" id="'. $ii .'">                                    
+                                // $button_html .= '<a class="button button-slide" id="'. $ii .'">
+                                //     <i class="fa fa-plus" aria-hidden="true"></i>
+                                //     <div style="color:'. $buttons[$ii]['button_color'] .';border:2px solid '. $buttons[$ii]['button_color'] .';">'. $buttons[$ii]['button_text'] .'</div>
+                                // </a>';
+
+
+                                //
+                                //     $slide_down_content = '<div class="slide-content-'. $ii .'">'. $buttons[$ii]['slide_down_content'] .'</div>';
+                                // } else {
+                                //     $button_html .= '<a class="button" target="'. $buttons[$ii]['target'] .'" href="'. $buttons[$ii]['button_link'] .'">
+                                //         <div style="color:'. $buttons[$ii]['button_color'] .';border:2px solid '. $buttons[$ii]['button_color'] .';">'. $buttons[$ii]['button_text'] .'</div>
+                                //     </a>';
+                                //
+                                //     $slide_down_content = '';
+
+
+                                // ------------- Create unique ids for collapsable button elements -- currently not selecting height element
+
+                                $button_html .= '<a class="button button-slide" id="'. $i .'-'. $ii .'">
                                     <i class="fa fa-plus" aria-hidden="true"></i>
                                     <div style="color:'. $buttons[$ii]['button_color'] .';border:2px solid '. $buttons[$ii]['button_color'] .';">'. $buttons[$ii]['button_text'] .'</div>
                                 </a>';
-                                
-                                $slide_down_content = '<div class="slide-content-'. $ii .'">'. $buttons[$ii]['slide_down_content'] .'</div>';
+
+                                $slide_down_content = '<div class="slide-content-'. $i .'-'. $ii .'">'. $buttons[$ii]['slide_down_content'] .'</div>';
                             } else {
                                 $button_html .= '<a class="button" target="'. $buttons[$ii]['target'] .'" href="'. $buttons[$ii]['button_link'] .'">
                                     <div style="color:'. $buttons[$ii]['button_color'] .';border:2px solid '. $buttons[$ii]['button_color'] .';">'. $buttons[$ii]['button_text'] .'</div>
                                 </a>';
-                                
+
                                 $slide_down_content = '';
                             }
 
                         }
                     }
-                    
+
                     $content = $content . $button_html;
-                    
+
                     if( $i % 2 == 0 ) {
-                        
+
                         echo '<div class="row row-even">
                                 <div class="col-md-6 alternating-content float-swap"><div>'. $content .'</div></div>
                                 <div class="col-md-6 alternating-image float-swap">'. $img .'</div>
@@ -75,7 +91,7 @@
                                 <div class="col-md-6 alternating-image float-swap"></div>
                             </div>';
                     } else {
-                        
+
                         echo '<div class="row row-odd">
                                 <div class="col-md-6 alternating-content"><div>'. $content .'</div></div>
                                 <div class="col-md-6 alternating-image">'. $img .'</div>
@@ -85,13 +101,13 @@
                                 <div class="col-md-6 alternating-image"></div>
                             </div>';
                     }
-                    
+
                 }
                 echo '</div>';
 
             endif;
 
             ?>
-            
+
         </div>
 </div>
